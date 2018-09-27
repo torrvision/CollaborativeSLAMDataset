408098d968fde629262929a525e3a264a63672ba
Stuart Golodetz <sgolodetz@gxstudios.net>
2018-09-27 16:31:49 +0100

    Continue working on the normal download script.

--- download-normal.sh
+++ download-normal.sh
@@ -2,7 +2,7 @@
 
 mkdir -p zips
 
-FILES='Flat-frames_resized-bathroom2study.zip Flat-frames_resized-kitchen2study.zip Flat-frames_resized-study2sittingroom.zip Flat-frames_resized-turret2sittingroom.zip Flat-meshes.zip House-frames_resized-diningroom2kitchen.zip House-frames_resized-frontroom2study.zip House-frames_resized-hall2frontbedroom.zip House-frames_resized-hall2oldkitchen.zip House-frames_resized-mainbedroom2studio.zip House-meshes.zip Lab-meshes.zip Priory-meshes.zip'
+FILES='Flat-frames_resized-bathroom2study.zip Flat-frames_resized-kitchen2study.zip Flat-frames_resized-study2sittingroom.zip Flat-frames_resized-turret2sittingroom.zip Flat-meshes.zip House-frames_resized-diningroom2kitchen.zip House-frames_resized-frontroom2study.zip House-frames_resized-hall2frontbedroom.zip House-frames_resized-hall2oldkitchen.zip House-frames_resized-mainbedroom2studio.zip House-meshes.zip Lab-frames_resized-atrium.zip Lab-frames_resized-firstfloor.zip Lab-frames_resized-groundfloor.zip Lab-frames_resized-secondfloor.zip Lab-meshes.zip Priory-frames_resized-bath2office.zip Priory-frames_resized-bed2office.zip Priory-frames_resized-dining2guest.zip Priory-frames_resized-guest2bath.zip Priory-frames_resized-kitchen2dining.zip Priory-frames_resized-living2dining.zip Priory-meshes.zip'
 
 for f in $FILES
 do
