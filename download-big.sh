bd077579d23faff67ff316728d819f67fe5ccfd9
Stuart Golodetz <sgolodetz@gxstudios.net>
2018-09-27 18:03:18 +0100

    Make the script for the big sequences.

---
+++ download-big.sh
@@ -0,0 +1,19 @@
+#! /bin/bash -e
+
+mkdir -p zips
+
+FILES='Flat-frames-bathroom2study.zip Flat-frames-kitchen2study.zip Flat-frames-study2sittingroom.zip Flat-frames-turret2sittingroom.zip House-frames-diningroom2kitchen.zip House-frames-frontroom2study.zip House-frames-hall2frontbedroom.zip House-frames-hall2oldkitchen.zip House-frames-mainbedroom2studio.zip Lab-frames-atrium.zip Lab-frames-firstfloor.zip Lab-frames-groundfloor.zip Lab-frames-secondfloor.zip Priory-frames-bath2office.zip Priory-frames-bed2office.zip Priory-frames-dining2guest.zip Priory-frames-guest2bath.zip Priory-frames-kitchen2dining.zip Priory-frames-living2dining.zip'
+
+for f in $FILES
+do
+	if [ ! -f "zips/$f" ]
+	then
+		echo "Downloading $f..."
+		curl -L "http://www.robots.ox.ac.uk/~tvg/projects/CollaborativeSLAM/bigdata/$f" > "zips/$f"
+
+		echo "Unzipping $f..."
+		unzip -qq "zips/$f"
+	else
+		echo "Skipping $f, already downloaded"
+	fi
+done
